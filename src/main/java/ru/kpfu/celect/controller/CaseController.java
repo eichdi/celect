ddc1e87fbab8b8b5bff2f5cca8fabcc8c4f70210
samat <khairutdinov.samat@gmail.com>
2016-12-13 18:06:34 +0300

    create controllers

--- src/main/java/ru/kpfu/celect/controller/CaseController.java
+++ src/main/java/ru/kpfu/celect/controller/CaseController.java
@@ -1,9 +1,12 @@
 package ru.kpfu.celect.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.kpfu.celect.dto.AuthDto;
+import ru.kpfu.celect.dto.CaseDto;
 import ru.kpfu.celect.dto.InterviewCasesDto;
+import ru.kpfu.celect.service.CelectService;
 
 /**
  * Created by Samat Khairutdinov on 12.12.16 18:58.
@@ -11,16 +14,18 @@ import ru.kpfu.celect.dto.InterviewCasesDto;
  */
 @RestController
 public class CaseController {
+    @Autowired
+    CelectService celectService;
 
     @RequestMapping(value = "/interview/{interview-id}/case", method = RequestMethod.GET)
     public ResponseEntity<InterviewCasesDto> getCases(@PathVariable("interview-id") int interviewId){
-        return null;
+        return new ResponseEntity<>(celectService.getCase(interviewId), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/interview/{interview-id}/case/{case-id}", method = RequestMethod.GET)
-    public ResponseEntity<InterviewCasesDto> election(@PathVariable("interview-id") int interviewId,
-                                                      @PathVariable("case-id") int caseId,
-                                                      @RequestHeader("phone") String phone){
-        return null;
+    public ResponseEntity<CaseDto> election(@PathVariable("interview-id") int interviewId,
+                                            @PathVariable("case-id") int caseId,
+                                            @RequestHeader("phone") String phone){
+        return new ResponseEntity<>(celectService.election(interviewId, caseId, phone), HttpStatus.OK);
     }
 }
--- src/main/java/ru/kpfu/celect/controller/UserController.java
+++ src/main/java/ru/kpfu/celect/controller/UserController.java
@@ -1,11 +1,14 @@
 package ru.kpfu.celect.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kpfu.celect.dto.AuthDto;
+import ru.kpfu.celect.service.CelectService;
 
 /**
  * Created by Samat Khairutdinov on 12.12.16 18:50.
@@ -13,14 +16,17 @@ import ru.kpfu.celect.dto.AuthDto;
  */
 @RestController
 public class UserController {
+    @Autowired
+    CelectService celectService;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ResponseEntity<AuthDto> login(@RequestHeader String phone){
-        return null;
+        return new ResponseEntity<>(celectService.login(phone), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public ResponseEntity<AuthDto> registration(@RequestHeader String phone){
-        return null;
+        return new ResponseEntity<>(celectService.registeration(phone), HttpStatus.OK);
     }
 
 
